e465367ac04ea3a0a725f75cec8e8a82f693c8c4
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-11-25 21:00:33 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("Введите операцию (add, subtract, multiply, divide) и два числа:");
+
+        String operation = scanner.next();
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+
+        try {
+            switch (operation.toLowerCase()) {
+                case "add":
+                    System.out.println("Результат: " + calculator.add(num1, num2));
+                    break;
+                case "subtract":
+                    System.out.println("Результат: " + calculator.subtract(num1, num2));
+                    break;
+                case "multiply":
+                    System.out.println("Результат: " + calculator.multiply(num1, num2));
+                    break;
+                case "divide":
+                    System.out.println("Результат: " + calculator.divide(num1, num2));
+                    break;
+                default:
+                    System.out.println("Неизвестная операция.");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
--- src/test/java/org/example/CalculatorTests.java
+++ src/test/java/org/example/CalculatorTests.java
@@ -1,2 +1,39 @@
-package org.example;public class CalculatorTests {
-}
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTests {
+    Calculator calculator = new Calculator();
+
+    public void shouldReturnPositiveResult_when_sumTwoNumbers() {
+        Double result = calculator.add(22.0, 23.0);
+        assertEquals(result, 45);
+    }
+        @Test
+        public void testSubtract() {
+            assertEquals(1.0, calculator.subtract(3.0, 2.0));
+        }
+
+        @Test
+        public void testMultiply() {
+            assertEquals(6.0, calculator.multiply(2.0, 3.0));
+        }
+
+        @Test
+        public void testDivide() {
+            assertEquals(2.0, calculator.divide(6.0, 3.0));
+        }
+        @Test
+        public void testDivideByZero() {
+            Exception exception = assertThrows(ArithmeticException.class, () -> {
+                calculator.divide(1.0, 0);
+            });
+
+            assertEquals("Деление на ноль невозможно.", exception.getMessage());
+        }
+
+    }
+
